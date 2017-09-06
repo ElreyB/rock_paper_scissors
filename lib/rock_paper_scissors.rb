78140bafccc191013074bcd5b22bf2e066fe2a92
Dan Nollette and Elrey Belmonti <elrey@>
2017-09-06 09:19:59 -0700

    keep track of games and print score

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,28 +4,42 @@ class String
   end
 end
 
-player_one = ""
-while (player_one != "rock") && (player_one != "paper") && (player_one != "scissors")
-  puts "Player one; what are you throwing?"
+player_one_wins = 0
+player_two_wins = 0
+ties = 0
+10.times{
 
-  player_one = gets.chomp
-end
+  player_one = ""
+  while (player_one != "rock") && (player_one != "paper") && (player_one != "scissors")
+    puts "Player one; what are you throwing?"
 
-100.times{ puts "__________"}
+    player_one = gets.chomp
+  end
 
-player_two = ""
+  100.times{ puts "__________"}
 
-while (player_two != "rock") && (player_two != "paper") && (player_two != "scissors")
-  puts "Player two; what are you throwing?"
+  player_two = ""
 
-  player_two = gets.chomp
-end
+  while (player_two != "rock") && (player_two != "paper") && (player_two != "scissors")
+    puts "Player two; what are you throwing?"
+
+    player_two = gets.chomp
+  end
 
 
-if player_one.beats?(player_two)
-  puts "Player one wins"
-elsif player_two.beats?(player_one)
-  puts "Player two wins"
-else
-  puts "Tie"
-end
+  if player_one.beats?(player_two)
+    player_one_wins += 1
+    puts "Player one wins"
+  elsif player_two.beats?(player_one)
+    player_two_wins += 1
+    puts "Player two wins"
+  else
+    ties += 1
+    puts "Tie"
+  end
+}
+
+puts "After 10 rounds:"
+puts "Player One win : #{player_one_wins}"
+puts "Player Two win : #{player_two_wins}"
+puts "Ties : #{ties}"
